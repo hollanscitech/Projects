46511579cb4d27f8656c19ac1fe0edb116c72a56
robert ugarte <robertu@roberts-MBP.lan1>
2020-06-09 14:30:24 -0500

    rush01.c

--- P02/rush01.c
+++ P02/rush01.c
@@ -6,7 +6,7 @@
 /* 
 F1 (ft_putchar) //writes characters
 F2 (formatArgv) //formats argv inputs
-F3 (putFormStr) //writes every character from formStr onto the standard output 
+F3 (putFormStr) //writes every value from a 2D character array onto the standard output
 */
 
 //F1
@@ -32,6 +32,40 @@ char *formatArgv(char *argv) {
 }
 
 //F3
+void printSudoku(char **sudoku) {
+  int x = 0;
+  int y = 0;
+  while(sudoku[y] != '\0') {
+    x = 0;
+    while(sudoku[y][x] != '\0') {
+      ft_putchar(sudoku[y][x]);
+      ft_putchar(' ');
+      x++;
+    }
+    ft_putchar('\n');
+    y++;
+  }
+}
+
+int main(int argc, char* argv[]) {
+  if (argc > 1 && argc <= 10) {
+    int z = 1;
+    char **sudoku;
+    sudoku = (char**) malloc(sizeof(char*) * (argc));
+    int y = 0;
+    while (argv[z] != NULL) {
+      sudoku[y] = formatArgv(argv[z]);
+      y++;
+      z++;
+    }
+    sudoku[y] = NULL;
+    printSudoku(sudoku);
+  }
+  return 0;
+}
+
+/*
+//F3
 void putFormStr(char *argv) {
   int i = 0;
   int argvLength = strlen(argv);
@@ -40,57 +74,11 @@ void putFormStr(char *argv) {
   while (i < argvLength) {
     ft_putchar(formStr[i]);
     if (i < (argvLength - 1)) {
-	ft_putchar(' ');
+        ft_putchar(' ');
       }
     i++;
   }
   ft_putchar('\n');
 }
-
-int main(int argc, char* argv[]) {
-  if (argc > 1 && argc <= 10) {
-    int y = 1;
-    while (argv[y] != NULL) {
-      formatArgv(argv[y]);
-      putFormStr(argv[y]);
-      y++;
-    }
-  }
-  return 0;
-}
-
-/*
-//F3
-char **makeSudoku(int argc, char *argv) {
-  char **sudoku;
-  sudoku = (char**) malloc(sizeof(char*) * (argc));
-  int i = 0;
-  int argvLength = argv[i];
-  char *str;
-  str = (char*) malloc(sizeof(char) * (argvLength + 1));
-  int y = 0;
-  while (argv) {
-    str[i] = argv[i];
-    i++;
-  }
-  str[i] = '\0';
-  sudoku[y] = str;
-  return sudoku;
-}
 */
-/*
-//F4
-void printSudoku(char **formArr) {
-  int x = 0;
-  int y = 0;
-  while(formArr[y] != NULL) {
-    x = 0;
-    while(formArr[y][x] != '\0') {
-      ft_putchar(formArr[y][x]);
-      x++;
-    }
-    ft_putchar('\n');
-    y++;
-  }
-}
-*/
+
