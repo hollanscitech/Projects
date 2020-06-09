c3fac830256dfee1134a61ec2b9b12b7c3c624ba
robert ugarte <robertu@roberts-MBP.lan1>
2020-06-09 09:37:08 -0500

    sastantua.c

--- P00/sastantua.c
+++ P00/sastantua.c
@@ -1,8 +1,30 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void ft_putchar(char a) {
   write(1, &a, 1);
+}
+
+int ft_atoi(char *str) {
+  int i = 0;
+  int number = 0;
+  int sign = 1;
+  while (str[i] != '\0' && (str[i] == '\n' || str[i] == '\t' || str[i] == ' ' || str[i] == '\r' || str[i] == '\f' || str[i] == '\v')) {
+    i++;
+  }
+  if (str[i] == '-') {
+    sign = -1;
+    i++;
+  }
+  if (str[i] == '+') {
+    i++;
+  }
+  while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9') {
+    number = (number * 10) + str[i] - 48;
+    i++;
+  }
+  return number * sign;
 }
 
 int ft_GetHeight(int size) {
@@ -32,6 +54,8 @@ int ft_GetWidth(int size) {
     }
     counter++;
   }
+  printf("%d\n", width);
+  printf("%d\n", difference);
   return width;
 }
 
@@ -42,18 +66,22 @@ void sastantua(int size) {
   int x = 0;
   while (y < height) {
     x = 0;
-    ft_putchar('/');
+    //ft_putchar('/');
     while (x < width) {
       ft_putchar('*');
       x++;
     }
-    ft_putchar(92);
+    //ft_putchar(92);
     ft_putchar('\n');
     y++;
   }
 }
 
-int main(void) {
-  sastantua(1);
-  return 0;
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    sastantua(ft_atoi(argv[1]));
+  }
+  else {
+    return 0;
+  }
 }
