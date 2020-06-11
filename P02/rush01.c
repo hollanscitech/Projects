b60c6179a7e2f12a144ef297daccc93b56201e61
robert ugarte <robertu@roberts-MBP.lan1>
2020-06-11 09:45:31 -0500

    rush01.c

--- P02/rush01.c
+++ P02/rush01.c
@@ -1,12 +1,15 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /* 
-F1 (ft_putchar) //writes characters
-F2 (formatArgv) //formats argv inputs
-F3 (putFormStr) //writes every value from a 2D character array onto the standard output
+F1 (ft_putchar)//writes characters
+F2 (formatArgv)//formats argv inputs
+F3 (checkRow)//determines if the character placement will break Sudoku rules for rows
+F4 (checkColumn)//determines if the character placement will break Sudoku rules for columns
+F5 (checkSubGrid)//determines if the character placement will break Sudoku rules for subgrids
+F6 (solveSudoku)//solves the sudoku 2D array
+F7 (putFormStr)//writes every value from a 2D character array onto the standard output
 */
 
 //F1
@@ -32,6 +35,11 @@ char *formatArgv(char *argv) {
 }
 
 //F3
+//F4
+//F5
+//F6
+
+//F7
 void printSudoku(char **sudoku) {
   int x = 0;
   int y = 0;
@@ -68,22 +76,3 @@ int main(int argc, char* argv[]) {
   }
   return 0;
 }
-
-/*
-//F3
-void putFormStr(char *argv) {
-  int i = 0;
-  int argvLength = strlen(argv);
-  char *formStr;
-  formStr = formatArgv(argv);
-  while (i < argvLength) {
-    ft_putchar(formStr[i]);
-    if (i < (argvLength - 1)) {
-        ft_putchar(' ');
-      }
-    i++;
-  }
-  ft_putchar('\n');
-}
-*/
-
